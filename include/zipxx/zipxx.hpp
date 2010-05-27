ed4d775aa2e43d12e0d3bad53b0269762909f4cb
Chris Pickel <sfiera@gmail.com>
2010-05-26 23:47:31 -0400

    Add zipxx/zipxx.hpp master header.

---
+++ include/zipxx/zipxx.hpp
@@ -0,0 +1,11 @@
+// Copyright (c) 2009 Chris Pickel <sfiera@gmail.com>
+//
+// This file is part of libzipxx, a free software project.  You can redistribute it and/or modify
+// it under the terms of the MIT License.
+
+#ifndef ZIPXX_ZIPXX_HPP_
+#define ZIPXX_ZIPXX_HPP_
+
+#include "zipxx/Zip.hpp"
+
+#endif  // ZIPXX_ZIPXX_HPP_
