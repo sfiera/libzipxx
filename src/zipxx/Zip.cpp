a0dfbcb0ec1ad731cb37d3ac991d8e64311ef70e
Chris Pickel <sfiera@twotaled.com>
2018-01-18 06:57:22 +0100

    Switch from libsfz to procyon

--- src/zipxx/Zip.cpp
+++ src/zipxx/Zip.cpp
@@ -7,18 +7,9 @@
 
 #include <errno.h>
 #include <zip.h>
-#include <sfz/sfz.hpp>
+#include <pn/file>
 
-using sfz::Bytes;
-using sfz::CString;
-using sfz::Exception;
-using sfz::PrintTarget;
-using sfz::String;
-using sfz::StringSlice;
-using sfz::format;
 using std::unique_ptr;
-
-namespace utf8 = sfz::utf8;
 
 namespace zipxx {
 
@@ -26,40 +17,35 @@ namespace {
 
 class AutoCloseZipFile {
   public:
-    AutoCloseZipFile(zip_file* file) : _file(file) { }
+    AutoCloseZipFile(zip_file* file) : _file(file) {}
     ~AutoCloseZipFile() { zip_fclose(_file); }
+
   private:
     zip_file* _file;
 };
 
 struct ZipErrorFormatter {
-    ZipErrorFormatter(int ze, int se)
-        : zip_error(ze),
-          system_error(se) { }
+    ZipErrorFormatter(int ze, int se) : zip_error(ze), system_error(se) {}
 
     const int zip_error;
     const int system_error;
 };
 
-void print_to(PrintTarget out, const ZipErrorFormatter& error) {
-    int size = zip_error_to_str(NULL, 0, error.zip_error, error.system_error);
+pn::string zip_error(int ze, int se) {
+    int                size = zip_error_to_str(NULL, 0, ze, se);
     unique_ptr<char[]> buf(new char[size + 1]);
-    zip_error_to_str(buf.get(), size + 1, error.zip_error, error.system_error);
-    print_to(out, buf.get());
+    zip_error_to_str(buf.get(), size + 1, ze, se);
+    return pn::string(buf.get());
 }
 
-ZipErrorFormatter zip_error(int ze, int se) {
-    return ZipErrorFormatter(ze, se);
-}
-
-ZipErrorFormatter zip_error(zip* archive) {
+pn::string zip_error(zip* archive) {
     int ze;
     int se;
     zip_error_get(archive, &ze, &se);
     return zip_error(ze, se);
 }
 
-ZipErrorFormatter zip_error(zip_file* file) {
+pn::string zip_error(zip_file* file) {
     int ze;
     int se;
     zip_file_error_get(file, &ze, &se);
@@ -68,14 +54,11 @@ ZipErrorFormatter zip_error(zip_file* file) {
 
 }  // namespace
 
-ZipArchive::ZipArchive(const StringSlice& path, int flags)
-    : _path(path) {
+ZipArchive::ZipArchive(const pn::string_view& path, int flags) : _path(path.copy()) {
     int error;
-    CString c_str(path);
-    const char* c_path = reinterpret_cast<const char*>(c_str.data());
-    _c_obj = zip_open(c_path, 0, &error);
+    _c_obj = zip_open(_path.c_str(), 0, &error);
     if (_c_obj == NULL) {
-        throw Exception(format("{0}: {1}", path, zip_error(error, errno)));
+        throw std::runtime_error(pn::format("{0}: {1}", path, zip_error(error, errno)).c_str());
     }
 }
 
@@ -85,24 +68,18 @@ ZipArchive::~ZipArchive() {
     }
 }
 
-const String& ZipArchive::path() const {
-    return _path;
-}
+pn::string_view ZipArchive::path() const { return _path; }
 
-zip* ZipArchive::c_obj() {
-    return _c_obj;
-}
+zip* ZipArchive::c_obj() { return _c_obj; }
 
-size_t ZipArchive::size() const {
-    return zip_get_num_files(_c_obj);
-}
+size_t ZipArchive::size() const { return zip_get_num_files(_c_obj); }
 
-ZipFileReader::ZipFileReader(ZipArchive& archive, const StringSlice& path) {
+ZipFileReader::ZipFileReader(ZipArchive& archive, const pn::string_view& path) {
     struct zip_stat st;
-    CString c_str(path);
-    if (zip_stat(archive.c_obj(), c_str.data(), 0, &st) != 0) {
-        throw Exception(format(
-                    "{0}: {1}: {2}", archive.path(), path, zip_error(archive.c_obj())));
+    if (zip_stat(archive.c_obj(), path.copy().c_str(), 0, &st) != 0) {
+        throw std::runtime_error(
+                pn::format("{0}: {1}: {2}", archive.path(), path, zip_error(archive.c_obj()))
+                        .c_str());
     }
     initialize(archive, st);
 }
@@ -110,35 +87,34 @@ ZipFileReader::ZipFileReader(ZipArchive& archive, const StringSlice& path) {
 ZipFileReader::ZipFileReader(ZipArchive& archive, int index) {
     struct zip_stat st;
     if (zip_stat_index(archive.c_obj(), index, 0, &st) != 0) {
-        throw Exception(format(
-                    "{0}: {1}: {2}", archive.path(), index, zip_error(archive.c_obj())));
+        throw std::runtime_error(
+                pn::format("{0}: {1}: {2}", archive.path(), index, zip_error(archive.c_obj()))
+                        .c_str());
     }
     initialize(archive, st);
 }
 
 void ZipFileReader::initialize(ZipArchive& archive, const struct zip_stat& st) {
-    _path.assign(utf8::decode(st.name));
+    _path = st.name;
 
     zip_file* file = zip_fopen_index(archive.c_obj(), st.index, 0);
     if (file == NULL) {
-        throw Exception(format(
-                    "{0}: {1}: {2}", archive.path(), _path, zip_error(archive.c_obj())));
+        throw std::runtime_error(
+                format("{0}: {1}: {2}", archive.path(), _path, zip_error(archive.c_obj()))
+                        .c_str());
     }
     AutoCloseZipFile close(file);
 
     _data.resize(st.size);
     int bytes_read = zip_fread(file, _data.data(), _data.size());
     if ((bytes_read < 0) || (bytes_read - _data.size() != 0)) {
-        throw Exception(format("{0}: {1}: {2}", archive.path(), _path, zip_error(file)));
+        throw std::runtime_error(
+                format("{0}: {1}: {2}", archive.path(), _path, zip_error(file)).c_str());
     }
 }
 
-const String& ZipFileReader::path() const {
-    return _path;
-}
+pn::string_view ZipFileReader::path() const { return _path; }
 
-const Bytes& ZipFileReader::data() const {
-    return _data;
-}
+pn::data_view ZipFileReader::data() const { return _data; }
 
 }  // namespace zipxx
